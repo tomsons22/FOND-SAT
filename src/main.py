9fdb29538980064104fb87b13cc74fd28d6cbbdd
Sebastian Sardina <ssardina@gmail.com>
2019-05-11 09:01:33 +0200

    Come cleanup and move tmp files to subdir; added option to leave it behind

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import shutil
+
 from CNF import CNF
 import os
 import sys
@@ -7,11 +9,13 @@ from timeit import default_timer as timer
 import time
 import argparse
 
+TMP_DIR = 'tmp'
+# TMP_LOGS_DIR = os.path.join(TMP_DIR, 'logs-run')
 
-def clean_and_exit(n1, n2, n3, n4, n5, msg):
+
+def clean(n1, n2, n3, n4, n5, msg):
     print(msg)
     os.system('rm %s %s %s %s %s' % (n1, n2, n3, n4, n5))
-    exit()
 
 
 def generateControllerStates(i):
@@ -47,7 +51,6 @@ args_parser.add_argument(
 args_parser.add_argument(
     '--strong',
     action='store_true',
-    dest='STRONG',
     default=False,
     help='Search for strong  solutions (instead of default strong cyclic solutions) - (default: %(default)s)')
 args_parser.add_argument(
@@ -58,55 +61,66 @@ args_parser.add_argument(
 args_parser.add_argument(
     '--gen-info',
     action='store_true',
-    dest='GEN_INFO',
     default=False,
     help='Show info about SAT formula generation %(default)s)')
 args_parser.add_argument(
     '--show-policy',
     action='store_true',
-    dest='SHOW_POLICY',
     default=False,
     help='Show final policy, if found %(default)s)')
+args_parser.add_argument(
+    '--no-clean',
+    action='store_true',
+    default=False,
+    help='Do not clean temporary files created %(default)s)')
 
 params = vars(args_parser.parse_args())  # vars returns a dictionary of the arguments
 
 print(params)  # just print the options that will be used
+
+if not os.path.exists(TMP_DIR):
+    os.makedirs(TMP_DIR)
 
 time_start = timer()
 time_limit = params['time_limit']
 time_buffer = 2
 mem_limit = params['mem_limit']
 
-print_policy = params['SHOW_POLICY']
-strong = params['STRONG']
-show_gen_info = params['GEN_INFO']
+print_policy = params['show_policy']
+strong = params['strong']
+show_gen_info = params['gen_info']
+no_clean = params['no_clean']
 
 
-p = Parser()
+# Let's now parse the PDDL domain and problem and generate SAS files (http://www.fast-downward.org/TranslatorOutputFormat)
+p = Parser()    # build utlity object Parser
 p.set_domain(params['path_domain'])
 p.set_problem(params['path_instance'])
+name_SAS_file = os.path.join(TMP_DIR, 'outputtrans-{}.txt'.format(params['name_temp']))                # aux file
+p.generate_file(name_SAS_file)
+p.generate_task(name_SAS_file)
 
-name_formula_file = 'formula-{}.txt'.format(params['name_temp'])
-name_formula_file_extra = 'formula-extra-{}.txt'.format(params['name_temp'])
-name_output_satsolver = 'outsat-{}.txt'.format(params['name_temp'])
-name_sas_file = 'outputtrans-{}.txt'.format(params['name_temp'])
-p.generate_file(name_sas_file)
-p.generate_task(name_sas_file)
+
+
 my_task = p.translate_to_atomic()
 fair = my_task.is_fair()
 
-
-cnf = CNF(name_formula_file, name_formula_file_extra, fair, strong)
+# Before we start iterationg, create a CNF object
+name_formula_file = os.path.join(TMP_DIR, 'formula-{}.txt'.format(params['name_temp']))                # aux file
+name_formula_file_extra = os.path.join(TMP_DIR, 'formula-extra-{}.txt'.format(params['name_temp']))    # aux file
+name_output_satsolver = os.path.join(TMP_DIR, 'outsat-{}.txt'.format(params['name_temp']))             # aux file
+cnf = CNF(name_formula_file, name_formula_file_extra, fair, strong)  # generate CNF formla into aux files
 
 solver_time = []
-for i in range(1000):
+for i in range(1000):   # try up to controller of size 1000
     if timer() - time_start > time_limit - time_buffer:
-        clean_and_exit(name_formula_file, name_output_satsolver, name_sas_file, name_formula_file_extra, name_final,
+        clean(name_formula_file, name_output_satsolver, name_SAS_file, name_formula_file_extra, name_final,
                        '-> OUT OF TIME')
 
+    # generate set of controller states (depending on iteration and scaling rate)
     controllerStates = generateControllerStates(i * params['inc'])
 
-    print('=================================================')
+    print('#######################################################################################')
     print('Trying with {} states'.format(len(controllerStates)))
     print('Looking for strong plans: {}'.format((strong)))
     print('Fair actions: {}'.format(str(fair)))
@@ -115,6 +129,9 @@ for i in range(1000):
 
     cnf.reset()
     start_g = timer()
+
+    ## NOW generate CNF for the particular size of the controller
+    # n0 and ng are the atoms for initial and goal controller states
     cnf.generate_clauses(my_task, 'n0', 'ng', controllerStates, len(controllerStates), p, show_gen_info)
 
     print('SAT formula generation time = {:f}'.format(timer() - start_g))
@@ -122,7 +139,7 @@ for i in range(1000):
     print('# Variables = {}'.format(cnf.getNumberVariables()))
 
     if timer() - time_start > time_limit - time_buffer:
-        clean_and_exit(name_formula_file, name_output_satsolver, name_sas_file, name_formula_file_extra, name_final,
+        clean(name_formula_file, name_output_satsolver, name_SAS_file, name_formula_file_extra, name_final,
                        '-> OUT OF TIME')
 
     print('Creating formula...')
@@ -133,9 +150,10 @@ for i in range(1000):
 
     time_for_sat = int(time_limit - (timer() - time_start))
     if time_for_sat < time_buffer:
-        clean_and_exit(name_formula_file, name_output_satsolver, name_sas_file, name_formula_file_extra, name_final,
+        clean(name_formula_file, name_output_satsolver, name_SAS_file, name_formula_file_extra, name_final,
                        '-> OUT OF TIME')
 
+    ## NOW, WE SOLVE THE SAT PROBLEM VIA MINISAT SOLVER (http://minisat.se/)
     # command = '/path/to/SATsolver/minisat -mem-lim=%i -cpu-lim=%i %s %s' % (mem_limit, time_for_sat, name_formula_file, name_output_satsolver)
     command = './minisat {} {}'.format(name_formula_file, name_output_satsolver)
 
@@ -148,7 +166,7 @@ for i in range(1000):
 
     result = cnf.parseOutput(name_output_satsolver, controllerStates, p, print_policy)
     if result is None:
-        clean_and_exit(name_formula_file, name_output_satsolver, name_sas_file, name_formula_file_extra, name_final,
+        clean(name_formula_file, name_output_satsolver, name_SAS_file, name_formula_file_extra, name_final,
                        '-> OUT OF TIME/MEM')
     if result:
         break
@@ -159,4 +177,11 @@ print('Elapsed solver time (s): {:f}'.format(sum(solver_time)))
 print('Elapsed solver time (s): {}'.format(str(solver_time)))
 print('Looking for strong plans: {}'.format(str(strong)))
 print('Fair actions: {}'.format(str(fair)))
-clean_and_exit(name_formula_file, name_output_satsolver, name_sas_file, name_formula_file_extra, name_final, 'Done')
+
+# clean up all auxiliarly files created
+if not no_clean:
+    clean(name_formula_file, name_output_satsolver, name_SAS_file, name_formula_file_extra, name_final, 'Done')
+    if os.path.exists(TMP_DIR):
+        shutil.rmtree(TMP_DIR)
+exit(0)
+
